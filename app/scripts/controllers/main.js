6603821ca7a65ebc997bf0ece63746e7e23a2d90
Selva <k.n.selvakumar@gmail.com>
2014-09-04 14:57:09 +0530

    clone value, so that dont update firebae back

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,7 +6,7 @@ angular.module('thanniThottiApp')
 
     syncData('meta/tank1').$asObject().$loaded().then(function(meta){
       $scope.$watch('tank1Readings', function(){
-        var tank1 = $scope.tank1Readings[Object.keys($scope.tank1Readings)[0]];
+        var tank1 = angular.copy($scope.tank1Readings[Object.keys($scope.tank1Readings)[0]]);
         angular.extend(tank1, meta);
         tank1.level = tank1.depth - tank1.distance;
         tank1.dateObj = new Date(tank1.timestamp * 1000);
