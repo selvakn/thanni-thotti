2f5823f792b0353cc10e6a36e6949e8e172f919c
Selva <k.n.selvakumar@gmail.com>
2014-09-04 14:35:24 +0530

    fixing lints

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,9 +3,9 @@
 angular
   .module('thanniThottiApp', ['angularfire.firebase', 'angularMoment'])
   .filter('capitalize', function() {
-    return function(input, all) {
+    return function(input) {
       return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
-    }
+    };
   })
   .filter('inFeet', function() {
     return function(input) {
@@ -14,5 +14,5 @@ angular
       var inchComponent = ((feet - feetComponent) * 12).toFixed(0);
 
       return feetComponent + '\' '+ inchComponent + '"';
-    }
+    };
   });
