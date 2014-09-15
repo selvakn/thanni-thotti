1836206a3b12bbf62c8994fd1836a35cedd3897c
Selva <k.n.selvakumar@gmail.com>
2014-09-15 20:11:41 +0530

    copy cat for tank2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,16 @@ angular.module('thanniThottiApp')
       }, true);
     });
 
+    syncData('readings/tank2', 1).$asObject().$bindTo($scope, 'tank2Readings');
+
+    syncData('meta/tank2').$asObject().$loaded().then(function(meta){
+      $scope.$watch('tank2Readings', function(){
+        var tank2 = angular.copy($scope.tank2Readings[Object.keys($scope.tank2Readings)[0]]);
+        angular.extend(tank2, meta);
+        tank2.level = tank2.depth - tank2.distance;
+        tank2.dateObj = new Date(tank2.timestamp * 1000);
+        $scope.tank2 = tank2;
+      }, true);
+    });
+
   });
